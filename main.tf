80cdd8fbdd4f789103fbde92b6fe304d3cb3837f
harsha1163 <88092816+harsha1163@users.noreply.github.com>
2021-08-06 20:08:30 +0530

    updated storage account name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
     }
     backend "azurerm" {
         resource_group_name  = "tfstate"
-        storage_account_name = "<storage_account_name>"
+        storage_account_name = "harshastorageaccount"
         container_name       = "tstate"
         key                  = "terraform.tfstate"
     }
